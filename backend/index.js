00fb8829fdb8aac0d44b2687867aa2d13507f2db
Jayesh Jain <saurabh@thesarya.com>
2025-07-02 22:27:13 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:8080',
+  origin: process.env.FRONTEND_URL || 'http://localhost:8080' || 'https://aaryavart-invoice.vercel.app',
   credentials: true
 }));
 
